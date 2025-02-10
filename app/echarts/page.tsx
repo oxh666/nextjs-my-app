abab337453930eb727a8da956b86cf148d12a283
oxh <106233851+oxh666@users.noreply.github.com>
2025-02-11 00:57:04 +0800

    fix: 1

--- app/echarts/page.tsx
+++ app/echarts/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 import React, { useState } from 'react';
-import EChartsComponent from './components/echarts-compoent';
 import EchartList from './components/EchartList';
 
-
-
 export default function DashboardPage(props: any) {
-
   return (
     <>
      <EchartList />
